b207e4d23c4b40f7eb4e11432d453b4ff70fdbb3
IhorLev1 <lambermen1@gmail.com>
2020-08-29 23:05:56 +0300

    add full homework

---
+++ js/task-8.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const createBoxRef = document.querySelector('[data-action="render"]');
+const deleteBoxRef = document.querySelector('[data-action="destroy"]');
+const boxes = document.querySelector("#boxes");
+
+function getRandomColor() {
+  const getRandomNumber = () => parseInt(Math.random() * 255);
+  const r = getRandomNumber();
+  const g = getRandomNumber();
+  const b = getRandomNumber();
+  const rgb = `${r},${g},${b}`;
+  return rgb;
+}
+
+function createBoxes(amount) {
+  let box = boxes.childElementCount;
+  for (box; box < amount; box += 1) {
+    const div = document.createElement("div");
+    div.style.width = `${30 + box * 10}px`;
+    div.style.height = `${30 + box * 10}px`;
+    div.style.backgroundColor = `rgb(${getRandomColor()})`;
+    boxes.appendChild(div);
+  }
+}
+
+function destroyBoxes() {
+  while (boxes.hasChildNodes()) {
+    boxes.removeChild(boxes.lastChild);
+  }
+}
+deleteBoxRef.addEventListener("click", () => {
+  destroyBoxes();
+});
+
+createBoxRef.addEventListener("click", () => {
+  createBoxes(document.querySelector("#controls").firstElementChild.value);
+});
